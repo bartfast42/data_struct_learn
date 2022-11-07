fda9bf0feeddbf185b2fa381cf94340ecb208a0e
bartfast42 <qqlestmelol@gmail.com>
2022-11-07 23:33:11 +0700

    remove a.out and change c_sockets

--- c_sockets/src/main.c
+++ c_sockets/src/main.c
@@ -1,37 +1,59 @@
+#include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-
-
-int getaddring( const char *node, //ip address or "www.example.com"
-                const char *service, //port number or "http"
-                const struct addrinfo *hints, 
-                struct addrinfo **res);
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 
 
+int main(int argc, char *argv[]) {
+    struct addrinfo hints, *res, *p;
+    int status;
+    char ipstr[INET6_ADDRSTRLEN];
 
-//example if server want to listen host ip at port 3490;
-int status;
-struct addrinfo hints;
-struct addrinfo *servinfo;
+    int s;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: showip hostname\n");
+        return 1;
+    }
+
+    memset(&hints, 0, sizeof hints);
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) !=0 ) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
+    }
+
+    printf("IP addresses for %s:\n\n", argv[1]);
+
+    for(p = res; p != NULL; p = p -> ai_next) {
+        void *addr;
+        char *ipver;
+        if (p -> ai_family == AF_INET) {
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else {
+            struct sockaddr_in6 *ipv6 = (struct sockarr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        inet_ntop(p -> ai_family, addr, ipstr, sizeof ipstr);
+        printf(" %s: %s\n", ipver, ipstr);
+    }
+
+    freeaddrinfo(res);
+
+    s = socket(res -> ai_family, res -> ai_socktype, res -> ai_protocol);
+    printf("%d \n", s);
+    return 0;
 
 
-memset(&hints, 0, sizeof hints);
-
-hints.ai_family = AF_UNSPEC;
-hints.ai_socktype = SOCK_STREAM;
-hints.ai_flags = AI_PASSIVE;
-
-
-if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
-    fprintf(stderr, "hetaddrinfo error: %s\n", gai_strerror(status));
-    exit(1);
 }
-
-
-
-freeaddrinfo(servinfo);
-
-
-
